54c797e940e6da29aa16ee8a09eb51d08f993fea
Sandeshcreator <77569129+Sandeshcreator@users.noreply.github.com>
2023-05-24 22:21:47 +0545

    Day 4

--- jewelry_shop-main/src/main/java/com/example/jewelry_store/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jewelry_store.repository;
-
-
-import com.example.jewelry_store.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
--- jewelry_shop-main/src/main/resources/static/frontend_ui/js/custom.js
+++
@@ -1,6 +0,0 @@
-function funSearch(){
-    var search_str=jQuery('#search_str').val();
-    if (search_str!=''){
-        window.location.href='/search/'+search_str;
-    }
-}
